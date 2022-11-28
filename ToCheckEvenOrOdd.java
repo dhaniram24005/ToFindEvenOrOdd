a4e242b524bf6117ca67fcb8dac704406b35001e
dhaniram24005 <87693749+dhaniram24005@users.noreply.github.com>
2022-11-28 23:42:17 +0530

    Add files via upload

---
+++ ToCheckEvenOrOdd.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ToCheckEvenOrOdd {
+    public static void main(String args[]) {
+
+        int num;
+        System.out.println("Enter an Integer no:");
+
+        // The input provided by the user is stored int num
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+
+        // if num is divisible by 2 then it is an even number
+        // else it is an odd no
+        if (num % 2 == 0) {
+            System.out.println(num + " :it is an even no:");
+        } else {
+            System.out.println(num + " :it is an odd no:");
+        }
+
+    }
+
+}
+/*
+ * Enter an integer no :
+ * 10
+ * 10: it is an even no
+ * 
+ * Enter an integer no:
+ * 11
+ * 11 : it is an odd no:
+ */
